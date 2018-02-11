e1ae6e3e77f8e20a779efe90ce46fe5903a55265
Yax <kianby@madyanne.fr>
2018-02-11 15:18:22 +0100

    subprocess and send back result

--- app/interface/rmqclient.py
+++ app/interface/rmqclient.py
@@ -11,16 +11,58 @@ from conf import config
 logger = logging.getLogger(__name__)
 
 
+credentials = pika.PlainCredentials(
+    config.rabbitmq['username'], config.rabbitmq['password'])
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.rabbitmq['host'], port=config.rabbitmq[
+                                     'port'], credentials=credentials, virtual_host=config.rabbitmq['vhost']))
+
+
+def mail(to_email, subject, message):
+
+    body = {
+        'to': to_email,
+        'subject': subject,
+        'content': message
+    }
+    channel = connection.channel()
+    channel.basic_publish(exchange=config.rabbitmq['exchange'],
+                          routing_key='mail.command.send',
+                          body=json.dumps(body, indent=False, sort_keys=False))
+    connection.close()
+    logger.info('Email for %s posted' % to_email)
+
+
+def send_delete_command(content):
+
+    channel = connection.channel()
+    channel.basic_publish(exchange=config.rabbitmq['exchange'],
+                          routing_key='mail.command.delete',
+                          body=json.dumps(content, indent=False, sort_keys=False))
+    connection.close()
+    logger.info('Email accepted. Delete request sent for %s' % content)
+
+
 def process_message(chan, method, properties, body):
     topic = method.routing_key
     data = json.loads(body)
 
-    if topic == 'mail.message': 
+    if topic == 'mail.message':
         logger.info('new message => {}'.format(data))
 
         for run in config.runs:
             if re.search(run['regex'], data['subject']):
-                logger.info('execute {}'.format(run))
+
+                try:
+                    r = subprocess.run(
+                        ["ls", "-l", "/dev/null"], stdout=subprocess.PIPE)
+                    message = str(r)
+                except:
+                    logger.exception('cannot execute')
+                    message = 'cannot execute {}'.format(run['exec'])
+
+                send_delete_command(data)
+
+                mail(data['from'], 'RE: ' + data['subject'], message)
                 break
             else:
                 logger.info('no match {} for {}'.format(data['subject'], run))
@@ -31,11 +73,6 @@ def process_message(chan, method, properties, body):
 class CommandConsumer(Thread):
 
     def run(self):
-
-        credentials = pika.PlainCredentials(
-            config.rabbitmq['username'], config.rabbitmq['password'])
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=config.rabbitmq['host'], port=config.rabbitmq[
-                                             'port'], credentials=credentials, virtual_host=config.rabbitmq['vhost']))
 
         channel = connection.channel()
         channel.exchange_declare(exchange=config.rabbitmq['exchange'],
